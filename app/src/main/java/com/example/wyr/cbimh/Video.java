7484743358b231beca33a4b3447103a66f1df3e6
Uuuuki <806277525@qq.com>
2018-03-28 11:44:41 +0800

    test

--- app/src/main/java/com/example/wyr/cbimh/Video.java
+++ app/src/main/java/com/example/wyr/cbimh/Video.java
@@ -1,16 +1,57 @@
 package com.example.wyr.cbimh;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by wyr on 2018/3/27.
  */
 
 public class Video extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_video);
+
+
+        toolbar.setTitle("宝贝视频");//设置Toolbar标题
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff")); //设置标题颜色
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // TODO Auto-generated method stub
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
